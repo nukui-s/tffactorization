bbebb622ad2e022114fb203d6590af8dd3995d1e
Nukui <nukui.s.aa@gmail.com>
2015-12-22 00:51:22 +0900

    integrate update operations into a step operation

--- tfnmf.py
+++ tfnmf.py
@@ -24,6 +24,8 @@ class TFNMF(object):
         scale = 2 * np.sqrt(V.mean() / rank)
         initializer = tf.random_uniform_initializer(maxval=scale)
 
+        graph = tf.get_default_graph()
+
         self.H = H = tf.get_variable("H", [rank, shape[1]],
                                      initializer=initializer)
         self.W = W = tf.get_variable(name="W", shape=[shape[0], rank],
@@ -31,46 +33,48 @@ class TFNMF(object):
 
         #save W for calculating delta with the updated W
         W_old = tf.get_variable(name="W_old", shape=[shape[0], rank])
-        self._save_W = W_old.assign(W)
+        save_W = W_old.assign(W)
 
         #Multiplicative updates
-        #update operation for H
-        Wt = tf.transpose(W)
-        WV = tf.matmul(Wt, V_)
-        WWH = tf.matmul(tf.matmul(Wt, W), H)
-        WV_WWH = WV / WWH
-        #select op should be executed in cpu not in gpu
-        with tf.device('/cpu:0'):
-            #convert nan to zero
-            WV_WWH = tf.select(tf.is_nan(WV_WWH),
-                                tf.zeros_like(WV_WWH),
-                                WV_WWH)
-        H_new = H * WV_WWH
-        self._update_H = H.assign(H_new)
+        with graph.control_dependencies([save_W]):
+            #update operation for H
+            Wt = tf.transpose(W)
+            WV = tf.matmul(Wt, V_)
+            WWH = tf.matmul(tf.matmul(Wt, W), H)
+            WV_WWH = WV / WWH
+            #select op should be executed in CPU not in GPU
+            with tf.device('/cpu:0'):
+                #convert nan to zero
+                WV_WWH = tf.select(tf.is_nan(WV_WWH),
+                                    tf.zeros_like(WV_WWH),
+                                    WV_WWH)
+            H_new = H * WV_WWH
+            update_H = H.assign(H_new)
 
+        with graph.control_dependencies([save_W, update_H]):
+            #update operation for W (after updating H)
+            Ht = tf.transpose(H)
+            VH = tf.matmul(V_, Ht)
+            WHH = tf.matmul(W, tf.matmul(H, Ht))
+            VH_WHH = VH / WHH
+            with tf.device('/cpu:0'):
+                VH_WHH = tf.select(tf.is_nan(VH_WHH),
+                                        tf.zeros_like(VH_WHH),
+                                        VH_WHH)
+            W_new = W * VH_WHH
+            update_W = W.assign(W_new)
 
-        #update operation for W (after updating H)
-        Ht = tf.transpose(H)
-        VH = tf.matmul(V_, Ht)
-        WHH = tf.matmul(W, tf.matmul(H, Ht))
-        VH_WHH = VH / WHH
-        with tf.device('/cpu:0'):
-            VH_WHH = tf.select(tf.is_nan(VH_WHH),
-                                    tf.zeros_like(VH_WHH),
-                                    VH_WHH)
-        W_new = W * VH_WHH
-        self._update_W = W.assign(W_new)
+        self.delta = tf.reduce_sum(tf.abs(W_old - W))
 
-        #sum of delta in W
-        self._delta = tf.reduce_sum(tf.abs(W_old - W))
+        self.step = tf.group(save_W, update_H, update_W)
 
     def run(self, sess, max_iter=200, min_delta=0.001):
         tf.initialize_all_variables().run()
         for i in range(max_iter):
-            sess.run(self._save_W)
-            H, _ = sess.run([self.H, self._update_H])
-            W, _ = sess.run([self.W, self._update_W])
-            delta = sess.run(self._delta)
+            self.step.run()
+            delta = self.delta.eval()
             if delta < min_delta:
                 break
+        W = self.W.eval()
+        H = self.H.eval()
         return W, H
